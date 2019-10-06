740e0e77c734a7ca90a62c3334ed0bc11c8d46f6
helonBot <51502205+helonBot@users.noreply.github.com>
2019-10-07 05:30:53 +0900

    Update test.js

--- test.js
+++ test.js
@@ -12,35 +12,8 @@ function bar() {
   newLi.appendChild(newAnchor);
   var list = document.getElementById("FavList");
   list.appendChild(newLi);
-  document.cookie = 'cookie_name=test'
+  document.cookie = 'cookie_name=test';
 }
 function username(user) {
   document.cookie = 'name=' + encodeURIComponent(user)+";helonbot.github.io; max-age=3600; secure; samesite=lax;";
-}
-// クッキー保存　setCookie(クッキー名, クッキーの値, クッキーの有効日数); //
-function setCookie(c_name,value,expiredays){
-  // pathの指定
-  var path = location.pathname;
-  // pathをフォルダ毎に指定する場合のIE対策
-  var paths = new Array();
-  paths = path.split("/");
-  if(paths[paths.length-1] != ""){
-      paths[paths.length-1] = "";
-      path = paths.join("/");
-  }
-  // 有効期限の日付
-  var extime = new Date().getTime();
-  var cltime = new Date(extime + (60*60*24*1000*expiredays));
-  var exdate = cltime.toUTCString();
-  // クッキーに保存する文字列を生成
-  var s="";
-  s += c_name +"="+ escape(value);// 値はエンコードしておく
-  s += "; path="+ path;
-  if(expiredays){
-      s += "; expires=" +exdate+"; ";
-  }else{
-      s += "; ";
-  }
-  // クッキーに保存
-  document.cookie=s;
 }
