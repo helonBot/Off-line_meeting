f755f83166dac8b5cd84f8a08f1aafe1eee533c2
helonBot <51502205+helonBot@users.noreply.github.com>
2019-10-07 15:52:26 +0900

    Update sample.js

--- sample.js
+++ sample.js
@@ -18,6 +18,61 @@ function username(user) {
 }
 function cl_color(day) {
   var newColor = document.getElementById(day);
+  window.onload = setCookie('days',day,30)
   newColor.setAttribute('bgcolor',"#9F9F9F");
   newColor.setAttribute('style',"color: #000000");
+}
+// クッキー保存　setCookie(クッキー名, クッキーの値, クッキーの有効日数); //
+function setCookie(c_name,value,expiredays){
+  // pathの指定
+  var path = location.pathname;
+  // pathをフォルダ毎に指定する場合のIE対策
+  var paths = new Array();
+  paths = path.split("/");
+  if(paths[paths.length-1] != ""){
+      paths[paths.length-1] = "";
+      path = paths.join("/");
+  }
+  // 有効期限の日付
+  var extime = new Date().getTime();
+  var cltime = new Date(extime + (60*60*24*1000*expiredays));
+  var exdate = cltime.toUTCString();
+  // クッキーに保存する文字列を生成
+  var s="";
+  s += c_name +"="+ escape(value);// 値はエンコードしておく
+  s += "; path="+ path;
+  if(expiredays){
+      s += "; expires=" +exdate+"; ";
+  }else{
+      s += "; ";
+  }
+  // クッキーに保存
+  document.cookie=s;
+}
+//cookieの設定
+function SWSetCookie(name, value, expire, path) {
+    var cookie = name + '=' + escape(value);
+    if(expire){
+        cookie += '; expires=' + expire.toGMTString();
+    }
+    if(path){
+        cookie += '; path=' + path;
+    }
+    document.cookie = cookie;
+}
+//cookieの取得
+function SWGetCookie(name){
+    var cookie = document.cookie;
+    if(cookie && cookie.length > 0){
+        var offset = cookie.indexOf(name + '=');
+        var end;
+        if(offset != -1){
+            offset += name.length + 1;
+            end     = cookie.indexOf(';',offset);
+            if(end == -1){
+                end = cookie.length;}
+            return unescape(cookie.substring(offset,end));
+          }
+    }
+    return "";
 }
